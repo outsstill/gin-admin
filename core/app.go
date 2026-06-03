f0f79a3665211044f69cbfc75d37eacc52fa04a8
zero <zhangzhuang@dongyou.com>
2026-06-03 16:41:49 +0800

    1.1.3

--- core/app.go
+++ core/app.go
@@ -3,10 +3,6 @@ package core
 import (
 	"github.com/outsstill/gin-admin/pkg/cache"
 	redisClient "github.com/outsstill/gin-admin/pkg/redis"
-	"github.com/outsstill/gin-admin/setting"
-	"github.com/redis/go-redis/v9"
-	"github.com/spf13/viper"
-	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
