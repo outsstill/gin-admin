c305a3b2a4232941876e31db2f1599d8709f0aac
zero <zhangzhuang@dongyou.com>
2026-06-22 15:57:51 +0800

    54

--- routes/admin.go
+++ routes/admin.go
@@ -10,6 +10,7 @@ import (
 	"github.com/outsstill/gin-admin/controllers"
 	"github.com/outsstill/gin-admin/core"
 	middlewares "github.com/outsstill/gin-admin/middlerwares"
+	"github.com/outsstill/gin-admin/pkg/response"
 	"github.com/outsstill/gin-admin/setting"
 )
 
@@ -138,10 +139,11 @@ func Setup404Handler(router *gin.Engine) {
 			c.String(http.StatusNotFound, "页面返回 404")
 		} else {
 			// 默认返回 JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
+			response.Abort404(c, "路由未定义，请确认 url 和请求方法是否正确。")
+			//c.JSON(http.StatusNotFound, gin.H{
+			//	"code":    404,
+			//	"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			//})
 		}
 	})
 }
